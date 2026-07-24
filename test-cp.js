83279db7d194368553ee6ef538baa38d870c7c51
Mowen559 <2477841445@qq.com>
2026-07-24 23:43:32 +0800

    优化

--- test-cp.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); fs.cpSync('hub-web/.next/standalone', 'test-standalone', { recursive: true }); console.log('Done!');
--- test-link.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); console.log('isSymbolicLink: ', fs.lstatSync('hub-web/.next/standalone/hub-web/node_modules/next').isSymbolicLink());
